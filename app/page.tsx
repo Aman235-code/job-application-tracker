464bbc9e741318121a06b28af2cd2ae7d3a09a88
Aman Ahamed <amanahamed04@gmail.com>
2026-01-20 21:06:02 +0530

    navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,38 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-   <div className="flex min-h-screen flex-col bg-white">
-    <main className="flex-1">
-      <section className="container mx-auto px-4 py-32">
-        <div className="mx-auto max-w-4xl text-center">
-          <h1 className="text-black mb-6 text-6xl font-bold ">A better way to track your job application</h1>
-          <p className="text-muted-foreground mb-10 text-xl">Capture, organize and manage your job search in one place</p>
-          <div className="flex flex-col items-center gap-4">
-            <Button size="lg" className="h-12 hover:cursor-pointer px-8 text-lg font-bold bg-black">Start for free<ArrowRight className="ml-2"/>
-            </Button>
-            <p className="text-sm text-muted-foreground">Free forever. No credit card required.</p>
+    <div className="flex min-h-screen flex-col bg-white">
+      <main className="flex-1">
+        <section className="container mx-auto px-4 py-32">
+          <div className="mx-auto max-w-4xl text-center">
+            <h1 className="text-black mb-6 text-6xl font-bold ">
+              A better way to track your job application
+            </h1>
+            <p className="text-muted-foreground mb-10 text-xl">
+              Capture, organize and manage your job search in one place
+            </p>
+            <div className="flex flex-col items-center gap-4">
+              <Link href={"/sign-up"}>
+                <Button
+                  size="lg"
+                  className="h-12 hover:cursor-pointer px-8 text-lg font-bold bg-primary"
+                >
+                  Start for free
+                  <ArrowRight className="ml-2" />
+                </Button>
+              </Link>
+
+              <p className="text-sm text-muted-foreground">
+                Free forever. No credit card required.
+              </p>
+            </div>
           </div>
-        </div>
-      </section>
-    </main>
-   </div>
+        </section>
+      </main>
+    </div>
   );
 }
