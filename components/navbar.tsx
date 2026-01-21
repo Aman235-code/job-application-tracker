3c2d010774064790f0bbd795a4c434654179edd7
Aman Ahamed <amanahamed04@gmail.com>
2026-01-21 14:48:26 +0530

    schema - board

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Briefcase } from "lucide-react";
 import Link from "next/link";
@@ -14,9 +14,7 @@ import SignOutButton from "./sign-out-btn";
 import { useSession } from "@/lib/auth/auth-client";
 
 export default function Navbar() {
-
-  const {data: session} = useSession()
- 
+  const { data: session } = useSession();
   return (
     <nav className="border-b border-gray-200 bg-white">
       <div className="container mx-auto flex h-16 items-center px-4 justify-between">
@@ -27,7 +25,6 @@ export default function Navbar() {
           <Briefcase />
           Job Tracker
         </Link>
-
         <div className="flex items-center gap-4">
           {session?.user ? (
             <>
@@ -41,36 +38,44 @@ export default function Navbar() {
               </Link>
               <DropdownMenu>
                 <DropdownMenuTrigger>
-                  <Button variant={"ghost"}>
-                    <Avatar>
+                  <Button
+                    variant="ghost"
+                    className="relative h-8 w-8 rounded-full"
+                  >
+                    <Avatar className="h-8 w-8">
                       <AvatarFallback className="bg-primary text-white">
                         {session.user.name[0].toUpperCase()}
                       </AvatarFallback>
                     </Avatar>
                   </Button>
                 </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <DropdownMenuLabel>
-                    <div>
-                      <p>{session.user.name}</p>
-                      <p>{session.user.email}</p>
+
+                <DropdownMenuContent className="w-56" align="end">
+                  <DropdownMenuLabel className="font-normal">
+                    <div className="flex flex-col space-y-1">
+                      <p className="text-sm font-medium leading-none">
+                        {session.user.name}
+                      </p>
+                      <p className="text-xs leading-none text-muted-foreground">
+                        {session.user.email}
+                      </p>
                     </div>
                   </DropdownMenuLabel>
-                 <SignOutButton/>
+                  <SignOutButton />
                 </DropdownMenuContent>
               </DropdownMenu>
             </>
           ) : (
             <>
-              <Link href={"/sign-in"}>
+              <Link href="/sign-in">
                 <Button
-                  variant={"ghost"}
+                  variant="ghost"
                   className="text-gray-700 hover:text-black"
                 >
                   Log In
                 </Button>
               </Link>
-              <Link href={"/sign-up"}>
+              <Link href="/sign-up">
                 <Button className="bg-primary hover:bg-primary/90">
                   Start for free
                 </Button>
